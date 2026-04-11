f2310137fc989394e13111b0eb02efa79522b595
Samir Haziel <samir.haziel@example.com>
2026-04-11 12:33:38 -0500

    Eliminar archivos obsoletos de Pokedex y sincronizar

--- semana04/mi-sitio-express/routes/pokedexRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const pokedexController = require("../controllers/pokedexController");
-
-router.get("/", pokedexController.list);
-router.post("/", pokedexController.create);
-
-module.exports = router;
